b72d64b530547838b8de8977d56f366d1743ea03
Malhar <=>
2023-11-04 17:12:32 +0530

    dweadg

--- controller/orderController.js
+++ controller/orderController.js
@@ -45,7 +45,7 @@ export const createOrder = async (req, res) => {
       from: "malhargamezone@gmail.com", // Sender's email address
       to: user.email, // Recipient's email address
       subject: "Your order details.",
-      text: "afdsgdhjkghfds",
+      text: updatedOrder.toString(),
     };
 
     // Send the email
