022128087ee4d4f371aac01a1ad55479b9f3588e
Kamlesh Patel <kamleshpatel12328@gmail.com>
2023-11-12 19:05:44 +0530

    bulk order api

--- controller/orderController.js
+++ controller/orderController.js
@@ -68,12 +68,44 @@ export const fetchAllOrders = async (req, res) => {
   }
 };
 
-export const updateOrder = async (req, res) => {
+export const updateAllOrders = async (req, res) => {
   try {
     const { storeManagerId, status } = req.body;
     const user = await User.findById(storeManagerId);
     user.orders.forEach((order, index) => {
       if (status) user.orders[index].status = status;
+    });
+    await user.save();
+    res.send({
+      success: true,
+      message: "Orders Updated Successfully",
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+export const updateBulkOrder = async (req, res) => {
+  try {
+    const { user_id, status, delivered } = req.body;
+    const userId = user_id;
+    const { bulkOrderId } = req.params;
+    const user = await User.findById(userId);
+    user.bulkOrders.forEach((bulkOrder, index) => {
+      if (bulkOrder._id.toString() === bulkOrderId) {
+        bulkOrder.orders.forEach((order, index2) => {
+          if (status) user.bulkOrders[index].orders[index2].status = status;
+          if (delivered) {
+            const lastDelivered =
+              user.bulkOrders[index].orders[index2].delivered;
+            user.bulkOrders[index].orders[index2].delivered =
+              lastDelivered + delivered;
+          }
+        });
+      }
     });
     await user.save();
     res.send({
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,7 +3,8 @@ import { authMiddleware } from "../middlewares/authMiddleware.js";
 import {
   createOrder,
   fetchAllOrders,
-  updateOrder,
+  updateAllOrders,
+  updateBulkOrder,
   updateSingleOrder,
 } from "../controller/orderController.js";
 
@@ -12,7 +13,8 @@ const router = Router();
 router
   .post("/", authMiddleware, createOrder)
   .get("/", authMiddleware, fetchAllOrders)
-  .put("/", authMiddleware, updateOrder)
+  .put("/", authMiddleware, updateAllOrders)
+  .put("/:bulkOrderId", authMiddleware, updateBulkOrder)
   .put("/:bulkOrderId/:orderId", authMiddleware, updateSingleOrder);
 
 export default router;
