288f7226350f8d777fba6d16a6fc80fc93e604ce
Kamlesh Patel <kamleshpatel12328@gmail.com>
2023-11-16 09:51:09 +0530

    notification apiadded timestamps

--- models/notificationModel.js
+++ models/notificationModel.js
@@ -1,25 +1,28 @@
 import mongoose from 'mongoose';
 
-const notificationSchema = new mongoose.Schema({
-  senderId: {
-    type: String,
-    required: true,
+const notificationSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: String,
+      required: true,
+    },
+    receiverId: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    isSeen: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  receiverId: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-    default: '',
-  },
-  isSeen: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 const Notification = mongoose.model('notifications', notificationSchema);
 export default Notification;
